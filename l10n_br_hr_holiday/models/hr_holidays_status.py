91fcd81848f0128c441126d61ac19c27bdd48d58
Hendrix Costa <hendrixcosta@gmail.com>
2016-12-22 11:21:32 -0200

    [ADD] Create Module

---
+++ l10n_br_hr_holiday/models/hr_holidays_status.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 KMEE INFORMATICA LTDA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models, _
+
+TYPE_DAY = [
+    ('uteis', u'Dias úteis Consecutivos'),
+    ('corridos', u'Dias corridos'),
+]
+
+class HrHolidaysStatus(models.Model):
+
+    _inherit = 'hr.holidays.status'
+
+    message = fields.Char(
+        string=u"Mensagem",
+    )
+
+    days_limit = fields.Integer(
+        string=u'Limite de Dias',
+    )
+
+    hours_limit = fields.Integer(
+        string=u'Limite de Horas',
+    )
+
+    type_day = fields.Selection(
+        string=u'Tipo de Dia',
+        selection=TYPE_DAY,
+    )
